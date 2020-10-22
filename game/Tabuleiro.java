0c5deeb51088eecc7a17b67e7531888e856f3447
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-22 20:20:53 -0300

    up

--- game/Tabuleiro.java
+++ game/Tabuleiro.java
@@ -1,5 +1,11 @@
 package game;
 
 public class Tabuleiro {
-    
+    int [][] tabuleiro = new int[5][5];
+
+    public void tabuleiro {
+        System.out.print("-----------------------------");
+        System.out.print("|    Antivírus por um dia    |");
+        System.out.print("");
+    }
 }
