fcd12f19553216d8cf0f66d34895ee76f390e0ae
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-22 20:41:06 -0300

    up

---
+++ game/GamePrincipal.java
@@ -0,0 +1,8 @@
+package game;
+public class GamePrincipal {
+    public static void main(String[] args) {
+        Tabuleiro t = new Tabuleiro();
+        t.gerarTabuleiro();
+
+    }
+}
--- game/Tabuleiro.java
+++ game/Tabuleiro.java
@@ -3,9 +3,18 @@ package game;
 public class Tabuleiro {
     int [][] tabuleiro = new int[5][5];
 
-    public void tabuleiro {
-        System.out.print("-----------------------------");
-        System.out.print("|    Antivírus por um dia    |");
-        System.out.print("");
+    public void gerarTabuleiro() {
+        System.out.println("-----------------------------");
+        System.out.println("|    Antivírus por um dia    |");
+        System.out.println("-----------------------------");
+        System.out.println("       1   2   3   4   5      ");
+       /* for(int linha = 0; linha <= tabuleiro.length; linha ++){
+            for(int coluna = 0; coluna <= tabuleiro[linha].length; coluna ++){
+                System.out.print(tabuleiro[linha][coluna] + "");
+            }
+        }
+        System.out.print("\n");
+        */
     }
+    
 }
