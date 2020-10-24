be44deff4edf4a0c42f08b470cc71f22e49ea472
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-24 20:11:36 -0300

    Update GamePrincipal.java

--- game/GamePrincipal.java
+++ game/GamePrincipal.java
@@ -3,6 +3,7 @@ public class GamePrincipal {
     public static void main(String[] args) {
         Tabuleiro jogo = new Tabuleiro();
         jogo.gerarTabuleiroInicial();
-        // continuar e criar 
+        // continuar e criar o tabuleiro do jogo
+        // 
     }
 }
