424962814d0279f77e887e294aa979056db9029a
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-26 20:35:49 -0300

    up

--- game/GamePrincipal.java
+++ game/GamePrincipal.java
@@ -2,11 +2,12 @@ package game;
 import java.util.Scanner;
 public class GamePrincipal {
     public static void main(String[] args) {
+
         int key;
         Tabuleiro jogo = new Tabuleiro();
         Scanner input = new Scanner(System.in);
         jogo.gerarTabuleiroInicial();
-        System.out.println("Vamos Começar o Jogo ? (1-sim / 2-não");
+        System.out.println("\nVamos Começar o Jogo ? (1-sim / 2-não)");
         key = input.nextInt();
         if(key == 1){
             jogo.gerarTabuleiroDeJogo();
