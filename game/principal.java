74548ea5aa6c6ae3baf34e67d92635b813916fc4
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-10-28 20:47:33 -0300

    Update principal.java

--- game/principal.java
+++ game/principal.java
@@ -2,6 +2,6 @@ package game;
 
 public class principal {
     public static void main(String[] args) {
-        placadecarro pq = new placadecarro("brasil",String[] a = {"a", "v"});
+        
     }   
 }
