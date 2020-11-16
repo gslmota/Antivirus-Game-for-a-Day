d20cd3a375ae35ce477004e4272f0b7701a4bd25
gslmota <gabrielsavio81@gmail.com>
2020-11-15 22:42:53 -0300

    up

--- game/game/Setor.java
+++ game/game/Setor.java
@@ -3,9 +3,12 @@ public class Setor {
     String parede = "|";
     //String [] vetorSetor = {"|", " ", parede, " ", parede, " ", parede, " ", parede, " ", "|"};
     String [] vetorSetor = {"|", " ", " ", " ",parede, " ", " ", " ",parede, " ", " ", " ",parede, " ", " ", " ",parede, " ", " ", " ","|"};
-    int posSetor[] = {3,3};
+    int posSetor[] = new int[2];
 
     public Setor() {
-
+        this.posSetor[0] = 3;
+        this.posSetor[1] = 3;
     }
+
+    
 }
